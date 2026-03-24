2f5dcd4f6757ae55103cf64001b699cedae936f9
Aishii09 <aishithakrai@gmail.com>
2026-03-24 12:03:13 +0530

    updated

--- server.js
+++ server.js
@@ -2,31 +2,35 @@ const connectDB = require("./config/db");
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const quizRoutes = require("./routes/quizRoutes");
 
-
-// Import routes
+// Routes
 const authRoutes = require("./routes/authRoutes");
-const attemptRoutes = require("./routes/attemptRoutes"); // add this AFTER authRoutes
+const quizRoutes = require("./routes/quizRoutes");
+const attemptRoutes = require("./routes/attemptRoutes");
+const adminRoutes = require("./routes/adminRoutes");
 
 dotenv.config();
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Connect Database
 connectDB();
 
-// Use routes
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/quiz", quizRoutes);
-app.use("/api/attempt", attemptRoutes); // use attempt routes
+app.use("/api/attempt", attemptRoutes);
+app.use("/api/admin", adminRoutes);
 
 // Test route
 app.get("/", (req, res) => {
-  res.send("Quiz Backend Running");
+  res.send("Backend Running Successfully 🚀");
 });
 
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
