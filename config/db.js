2b9fd9d64c2a575b0fcf1785fe07ad79d37dbc0f
Aishii09 <aishithakrai@gmail.com>
2026-06-01 15:06:10 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (error) {
-    console.log("DB error");
+    console.log("DB error:", error.message);
   }
 };
 
